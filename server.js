034b7fa53238d774f4e5ae606ad2904ac411033d
Ramakrishna <132547350+RAMAKRISHNA3488@users.noreply.github.com>
2025-12-01 23:40:30 +0530

    rk

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ connectDB();
 // Middleware
 app.use(cors({
     origin: process.env.NODE_ENV === 'production'
-        ? 'https://your-production-domain.com'
+        ? 'https://amey-bistro-frontend.vercel.app/'
         : 'http://localhost:5173',
     credentials: true
 }));
